61e45ca2fe6a38992969b46ceea2494ec84acc83
Tod Hansmann <githubpub@todandlorna.com>
2023-08-22 19:56:42 -0600

    feat: add clubdoers knowing it will fail for distribution id problems for next PR

--- terraform/site/main.tf
+++ terraform/site/main.tf
@@ -1,12 +1,14 @@
 locals {
-  environment_name        = "opsisdead-com" # THIS MUST BE UPDATED IF COPIED
-  environment_domain_name = "opsisdead.com"
+  opsisdead_environment_name        = "opsisdead-com" # THIS MUST BE UPDATED IF COPIED
+  opsisdead_environment_domain_name = "opsisdead.com"
+  clubdoers_environment_name        = "clubdoers-com" # THIS MUST BE UPDATED IF COPIED
+  clubdoers_environment_domain_name = "clubdoers.com"
 }
 
 # manage secrets for this environment via aws secrets manager for secret
 # versioning and aws access control over secrets
 data "aws_secretsmanager_secret_version" "env" {
-  secret_id = "tnlcommunity/static-sites/${local.environment_name}"
+  secret_id = "tnlcommunity/static-sites/${local.opsisdead_environment_name}"
   provider  = aws
 }
 
@@ -17,13 +19,13 @@ locals {
 }
 
 data "cloudflare_zone" "opsisdead-com" {
-  name = local.environment_domain_name
+  name = local.opsisdead_environment_domain_name
 }
 
 module "demo-acm" {
   source = "../modules/acm-with-cloudflare/"
 
-  domain_name        = local.environment_domain_name
+  domain_name        = local.opsisdead_environment_domain_name
   cloudflare_zone_id = data.cloudflare_zone.opsisdead-com.id
 
   providers = {
@@ -31,12 +33,13 @@ module "demo-acm" {
   }
 }
 
+
 module "opsisdead-com-static-site" {
   source  = "catalystsquad/staticsite/aws"
   version = "~> 1.1.0"
 
-  website_domain = local.environment_domain_name
-  bucket_name    = local.environment_domain_name
+  website_domain = local.opsisdead_environment_domain_name
+  bucket_name    = local.opsisdead_environment_domain_name
 
   acm_certificate_arn    = module.demo-acm.acm_certificate_arn
   create_acm_certificate = false
@@ -57,8 +60,55 @@ module "opsisdead-com-static-site" {
 
 resource "cloudflare_record" "opsisdead-com" {
   zone_id = data.cloudflare_zone.opsisdead-com.id
-  name    = local.environment_domain_name
+  name    = local.opsisdead_environment_domain_name
   value   = module.opsisdead-com-static-site.cloudfront_domain_name
+  type    = "CNAME"
+  ttl     = 300
+  proxied = false
+}
+
+data "cloudflare_zone" "clubdoers-com" {
+  name = local.clubdoers_environment_domain_name
+}
+
+module "demo-acm" {
+  source = "../modules/acm-with-cloudflare/"
+
+  domain_name        = local.clubdoers_environment_domain_name
+  cloudflare_zone_id = data.cloudflare_zone.clubdoers-com.id
+
+  providers = {
+    aws = aws.acm
+  }
+}
+module "clubdoers-com-static-site" {
+  source  = "catalystsquad/staticsite/aws"
+  version = "~> 1.1.0"
+
+  website_domain = local.clubdoers_environment_domain_name
+  bucket_name    = local.clubdoers_environment_domain_name
+
+  acm_certificate_arn    = module.demo-acm.acm_certificate_arn
+  create_acm_certificate = false
+  create_site_records    = false
+
+  cloudfront_custom_error_responses = [{
+    error_caching_min_ttl = 300
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }]
+
+  providers = {
+    aws.main = aws
+    aws.acm  = aws.acm
+  }
+}
+
+resource "cloudflare_record" "clubdoers-com" {
+  zone_id = data.cloudflare_zone.clubdoers-com.id
+  name    = local.clubdoers_environment_domain_name
+  value   = module.clubdoers-com-static-site.cloudfront_domain_name
   type    = "CNAME"
   ttl     = 300
   proxied = false
